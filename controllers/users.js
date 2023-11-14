590092693e28da095e62fb3b269199573c31a855
Richard Leshik <richardleshik@gmail.com>
2023-11-14 16:12:44 +0300

    fix login function

--- controllers/users.js
+++ controllers/users.js
@@ -20,12 +20,14 @@ const login = async (req, res) => {
   })
 
   const isPasswordCorrect = user && (await bcrypt.compare(password, user.password))
+  const secret = process.env.JWT_SECRET
 
-  if (user && isPasswordCorrect) {
+  if (user && isPasswordCorrect && secret) {
     res.status(200).json({
       id: user.id,
       email: user.email,
-      name: user.name
+      name: user.name,
+      token: jwt.sign({id: user.id}, secret, {expiresIn: '30d'})
     })
   } else {
     return res.status(400).json({message: 'Incorrect login or password'})
