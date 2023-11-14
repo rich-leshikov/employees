ccf3ab4f749c36ce242c78b8754c9c1d4b33c080
Richard Leshik <richardleshik@gmail.com>
2023-11-14 14:47:41 +0300

    add register function

--- controllers/users.js
+++ controllers/users.js
@@ -32,8 +32,50 @@ const login = async (req, res) => {
   }
 }
 
+/**
+ *
+ * @route POST /api/user/register
+ * @desc Registration
+ * @access Public
+ */
 const register = async (req, res) => {
-  res.send('register')
+  const {email, password, name} = req.body
+
+  if (!email || !password || !name) {
+    return res.status(400).json({message: 'Please fill in the required fields'})
+  }
+
+  const registeredUser = await prisma.user.findFirst({
+    where: {email}
+  })
+
+  if (registeredUser) {
+    return res.status(400).json({message: 'User with similar email already exists'})
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword
+    }
+  })
+
+  const secret = process.env.JWT_SECRET
+
+  if (user && secret) {
+    res.status(201).json({
+      id: user.id,
+      email: user.email,
+      name,
+      token: jwt.sign({id: user.id}, secret, {expiresIn: '30d'})
+    })
+  } else {
+    return res.status(400).json({message: 'Failed to create user'})
+  }
 }
 
 const current = async (req, res) => {
