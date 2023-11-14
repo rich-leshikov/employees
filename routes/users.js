c1abd73833889736f7c88b18f439be21925749af
Richard Leshik <richardleshik@gmail.com>
2023-11-14 18:18:54 +0300

    update users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const {login, register, current} = require('../controllers/users')
+const {auth} = require('../middleware/auth')
 
+// /api/user/login
 router.post('/login', login)
+// /api/user/register
 router.post('/register', register)
-router.get('/current', current)
+// /api/user/current
+router.get('/current', auth, current)
 
 module.exports = router
