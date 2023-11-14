2a29f62142cb6bf16ac2d8f693b01430d09c0b7d
Richard Leshik <richardleshik@gmail.com>
2023-11-14 22:03:26 +0300

    add adding employee function

--- controllers/employees.js
+++ controllers/employees.js
@@ -12,10 +12,38 @@ const all = async (req, res) => {
 
     res.status(200).json(employees)
   } catch (err) {
-    res.status(400).json({message: 'Can not get employees'})
+    res.status(500).json({message: 'Can not get employees'})
+  }
+}
+
+/**
+ *
+ * @route POST /api/employees/add
+ * @desc Adding an employee
+ * @access Private
+ */
+const add = async (req, res) => {
+  const data = req.body
+
+  try {
+    if (!data.firstName || !data.lastName || !data.address || !data.age) {
+      return res.status(400).json({message: 'All fields require'})
+    }
+
+    const employee = await prisma.employee.create({
+      data: {
+        ...data,
+        userId: req.user.id
+      }
+    })
+
+    return res.status(201).json(employee)
+  } catch (err) {
+    res.status(500).json({message: 'Something went wrong'})
   }
 }
 
 module.exports = {
-  all
+  all,
+  add
 }
--- routes/employees.js
+++ routes/employees.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const {auth} = require('../middleware/auth')
-const {all} = require('../controllers/employees')
+const {all, add} = require('../controllers/employees')
 
 // /api/employees
 router.get('/', auth, all)
 // /api/employees/:id
 router.get('/:id', auth, () => console.log('get single employee'))
 // /api/employees/add
-router.post('/add', auth, () => console.log('add employee'))
+router.post('/add', auth, add)
 // /api/employees/remove/:id
 router.delete('/remove/:id', auth, () => console.log('remove employee'))
 // /api/employees/edit/:id
