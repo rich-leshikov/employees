ce382c42cf16517dbd5582d996f89ef690c0661d
Richard Leshik <richardleshik@gmail.com>
2023-11-15 23:18:25 +0300

    add browser router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
+import {createRoot} from 'react-dom/client'
+import {Provider} from 'react-redux'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {store} from './app/store'
+import {Paths} from './paths'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h2>Home page</h2>
+  }, {
+    path: Paths.login,
+    element: <h2>Login</h2>
+  }, {
+    path: Paths.register,
+    element: <h2>Register</h2>
+  }
+])
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      App
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 )
