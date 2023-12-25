dc259d5d8e77b13ba1b7d53b1ebad661d9ae6314
Richard Leshik <richardleshik@gmail.com>
2023-12-26 00:36:54 +0300

    feat: add icons to navigation buttons

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react'
 import {Link} from 'react-router-dom'
 import {Layout, Space, Typography} from 'antd'
-import {TeamOutlined} from '@ant-design/icons'
+import {LoginOutlined, TeamOutlined, UserOutlined} from '@ant-design/icons'
 import s from './index.module.css'
 import {CustomButton} from '../custom-button'
 import {Paths} from '../../paths'
@@ -14,17 +14,17 @@ export const Header: FC<HeaderProps> = () => {
       <Space>
         <TeamOutlined className={s.teamIcon}/>
         <Link to={Paths.home}>
-          <CustomButton type={'link'}>
+          <CustomButton type={'ghost'}>
             <Typography.Title level={1}>Employees</Typography.Title>
           </CustomButton>
         </Link>
       </Space>
       <Space>
         <Link to={Paths.register}>
-          <CustomButton type={'link'}>Sign Up</CustomButton>
+          <CustomButton type={'ghost'} icon={<UserOutlined/>}>Sign Up</CustomButton>
         </Link>
         <Link to={Paths.login}>
-          <CustomButton type={'link'}>Sign In</CustomButton>
+          <CustomButton type={'ghost'} icon={<LoginOutlined/>}>Sign In</CustomButton>
         </Link>
       </Space>
     </Layout.Header>
