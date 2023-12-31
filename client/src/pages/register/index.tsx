97c98e235a551cacc49a27964c6b1ca6d72a347b
Richard Leshik <richardleshik@gmail.com>
2023-12-31 23:24:37 +0300

    feat: add register form to Register component

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import {Layout} from '../../components/layout'
+import {Card, Form, Row, Space, Typography} from 'antd'
+import {CustomInput} from '../../components/custom-input'
+import {PasswordInput} from '../../components/password-input'
+import {CustomButton} from '../../components/custom-button'
+import {Link} from 'react-router-dom';
+import {Paths} from '../../paths';
 
 export const Register = () => {
   return (
     <Layout>
-      <div>Register</div>
+      <Row align={'middle'} justify={'center'}>
+        <Card title={'Sign Up'} style={{width: '30rem'}}>
+          <Form onFinish={() => null}>
+            <CustomInput name={'name'} placeholder={'Name'}/>
+            <CustomInput type={'email'} name={'email'} placeholder={'Email'}/>
+            <PasswordInput name={'password'} placeholder={'Password'}/>
+            <PasswordInput name={'confirmPassword'} placeholder={'Confirm your password'}/>
+            <CustomButton type={'primary'} htmlType={'submit'}>Sign Up</CustomButton>
+          </Form>
+          <Space direction={'vertical'} size={'large'}>
+            <Typography.Text>
+              Have already registered? <Link to={Paths.login}>Sign In</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
   )
 }
