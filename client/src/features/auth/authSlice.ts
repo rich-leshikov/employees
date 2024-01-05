ca65bb38994870b76370b2b3f295803eb689c4ab
Richard Leshik <richardleshik@gmail.com>
2024-01-05 16:36:31 +0300

    feat: add auth slice

---
+++ client/src/features/auth/authSlice.ts
@@ -0,0 +1,42 @@
+import {User} from '@prisma/client'
+import {createSlice} from '@reduxjs/toolkit'
+import {authApi} from '../../app/services/auth'
+import {RootState} from '../../app/store'
+
+interface InitialState {
+  user: User & { token: string } | null
+  isAuth: boolean
+}
+
+const initialState: InitialState = {
+  user: null,
+  isAuth: false
+}
+
+const slice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logout: () => initialState
+  },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(authApi.endpoints.login.matchFulfilled, (state, action) => {
+        state.user = action.payload
+        state.isAuth = true
+      })
+      .addMatcher(authApi.endpoints.register.matchFulfilled, (state, action) => {
+        state.user = action.payload
+        state.isAuth = true
+      })
+      .addMatcher(authApi.endpoints.current.matchFulfilled, (state, action) => {
+        state.user = action.payload
+        state.isAuth = true
+      })
+  }
+})
+
+export const {logout} = slice.actions
+export default slice.reducer
+export const selectIsAuth = (state: RootState) => state.auth.isAuth
+export const selectUser = (state: RootState) => state.auth.user
