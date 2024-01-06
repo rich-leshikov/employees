611073b60af54e26de2ac85ad33fea0dffbacca0
Richard Leshik <richardleshik@gmail.com>
2024-01-06 21:38:09 +0300

    feat: add login function for Login page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {Card, Form, Row, Space, Typography} from 'antd'
 import {Layout} from '../../components/layout'
@@ -5,13 +6,32 @@ import {CustomInput} from '../../components/custom-input'
 import {PasswordInput} from '../../components/password-input'
 import {CustomButton} from '../../components/custom-button'
 import {Paths} from '../../paths'
+import {useLoginMutation, UserData} from '../../app/services/auth'
+import {isErrorMessage} from '../../utils/is-error-message'
 
 export const Login = () => {
+  const [loginUser, loginUserResult] = useLoginMutation()
+  const [error, setError] = useState('')
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap()
+    } catch (err) {
+      const maybeError = isErrorMessage(err)
+
+      if (maybeError) {
+        setError(err.data.message)
+      } else {
+        setError('Unknown error')
+      }
+    }
+  }
+
   return (
     <Layout>
       <Row align={'middle'} justify={'center'}>
         <Card title={'Sign In'} style={{width: '30rem'}}>
-          <Form onFinish={() => null}>
+          <Form onFinish={login}>
             <CustomInput type="email" name="email" placeholder="Email"/>
             <PasswordInput name="password" placeholder="Password"/>
             <CustomButton type="primary" htmlType="submit">Sign In</CustomButton>
@@ -26,3 +46,4 @@ export const Login = () => {
     </Layout>
   )
 }
+
