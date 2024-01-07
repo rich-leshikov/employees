532aafe5ce87438c871c66c899c9dffb5b44b1f2
Richard Leshik <richardleshik@gmail.com>
2024-01-07 23:54:20 +0300

    feat: add headers with token to baseQuery

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -1,7 +1,16 @@
 import {createApi, fetchBaseQuery, retry} from '@reduxjs/toolkit/query/react'
+import {RootState} from '../store';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:8000/api'
+  baseUrl: 'http://localhost:8000/api',
+  prepareHeaders(headers, {getState}) {
+    const token = (getState() as RootState).auth.user?.token ||
+      localStorage.getItem('token')
+
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`)
+    }
+  }
 })
 
 const baseQueryWithRetry = retry(baseQuery, {maxRetries: 1})
