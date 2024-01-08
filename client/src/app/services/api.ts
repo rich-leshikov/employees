c8e1ec844d837bbad636e3a24431b2011accd0a5
Richard Leshik <richardleshik@gmail.com>
2024-01-08 23:54:53 +0300

    feat: add employees API

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery, retry} from '@reduxjs/toolkit/query/react'
-import {RootState} from '../store';
+import {RootState} from '../store'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'http://localhost:8000/api',
