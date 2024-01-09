35d7b5b2368c4b434fd3a48550caa4d5dbc09892
Richard Leshik <richardleshik@gmail.com>
2024-01-09 18:40:43 +0300

    feat: add employees slice

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,12 +1,14 @@
 import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit'
 import auth from '../features/auth/authSlice'
+import employees from '../features/employees/employeesSlice'
 import {api} from './services/api'
 import {listenerMiddleware} from '../middleware/auth';
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
-    auth
+    auth,
+    employees
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware).prepend(listenerMiddleware.middleware)
 })
